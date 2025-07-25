d30b38cd6a50e32456e70218ef367b1af933c615
Juan Benicio <juanpablobenicio@gmail.com>
2025-07-25 17:50:23 -0300

    Add Not Found Middleware

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+    //res.json({'message': "Api en mantenimiento!!"})
+    console.log(req.method);
+    next();
+})
+
 app.get('/', (req, res) => {
-    res.send("Api Rest!!");
+    res.json({'message': 'Api Rest!!'});
 });
+
+import notFound from "./src/middleware/not-found.js";
+app.use(notFound);
 
 const port= 3000;
 
